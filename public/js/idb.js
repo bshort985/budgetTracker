35acc8ce473a6c9e2ef6c2f2394e8940b4cbb3f1
bshort985 <97572888+bshort985@users.noreply.github.com>
2022-07-04 18:27:54 -0400

    started indexDB

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,3 +1,22 @@
 let db;
 const request = indexedDB.open("budget", 1);
 
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+
+request.onupgradeneeded = function({ target }) {
+    const db = target.result;
+    db.createObjectStore("entry", { autoIncrement: true });
+};
+
+request.onsuccess = funnction({ target }) {
+    db = target.result;
+
+    if (navigator.onLine) {
+        // uploadEntry();
+    }
+};
+
+request.onerror = function({ target }) {
+    // log error here
+    console.log(target.errorCode);
+}
