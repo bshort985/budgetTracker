fd0cc360074ccde4023ad97f0a1b1414f370f7b2
bshort985 <97572888+bshort985@users.noreply.github.com>
2022-07-04 17:28:42 -0400

    finished service worker and manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -19,13 +19,50 @@ const FILES_TO_CACHE = [
     "./icons/icon-512x512.png"
   ];
 
-  // Cache resources
+  // install service worker
+
 self.addEventListener("install", function(event) {
-    // Perform install steps
     event.waitUntil(
       caches.open(CACHE_NAME).then(function(cache) {
         console.log('installing cache : ' + CACHE_NAME)
         return cache.addAll(FILES_TO_CACHE);
       })
     );
+  });
+
+
+  // Activate and delete old chaches
+
+  self.addEventListener('activate', function (e) {
+    e.waitUntil(
+      caches.keys().then(function (keyList) {
+        let cacheKeeplist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX);
+        })
+        cacheKeeplist.push(CACHE_NAME);
+        return Promise.all(keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i] );
+            return caches.delete(keyList[i]);
+          }
+        }));
+      })
+    );
+  });
+
+   // Respond with cached resources
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { 
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else { 
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+      })
+    )
   });
