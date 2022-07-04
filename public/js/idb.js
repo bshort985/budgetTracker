409f54173190070afb29b3ad88410f1e0a8924cb
bshort985 <97572888+bshort985@users.noreply.github.com>
2022-07-04 19:55:40 -0400

    added offline data collection

--- public/js/idb.js
+++ public/js/idb.js
@@ -8,15 +8,59 @@ request.onupgradeneeded = function({ target }) {
     db.createObjectStore("entry", { autoIncrement: true });
 };
 
-request.onsuccess = funnction({ target }) {
+request.onsuccess = function({ target }) {
     db = target.result;
-
     if (navigator.onLine) {
-        // uploadEntry();
+        uploadEntry();
     }
 };
 
 request.onerror = function({ target }) {
     // log error here
     console.log(target.errorCode);
+};
+
+// This function will be executed if we attempt to submit a new entry and there's no internet connection
+
+function saveRecord(record) {
+    const transaction = db.transaction(["entry"], "readwrite");
+    const entryObjectStore = transaction.objectStore("entry");
+    entryObjectStore.add(record);
 }
+
+function uploadEntry() {
+    const transaction = db.transaction(["entry"], "readwrite");
+    const entryObjectStore = transaction.objectStore("entry");
+    const getAll = entryObjectStore.getAll();
+    getAll.onsuccess = function() {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+          fetch('/api/transaction/bulk', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: 'application/json, text/plain, */*',
+              'Content-Type': 'application/json'
+            }
+          })
+            .then(response => response.json())
+            .then(serverResponse => {
+              if (serverResponse.message) {
+                throw new Error(serverResponse);
+              }
+              // open one more transaction
+              const transaction = db.transaction(["entry"], 'readwrite');
+              const entryObjectStore = transaction.objectStore("entry");
+              // clear all items in your store
+              entryObjectStore.clear();
+    
+              alert('All saved entries have been submitted!');
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }
+      };
+}
+
+window.addEventListener('online', uploadEntry);
